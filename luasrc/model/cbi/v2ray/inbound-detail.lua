ab9f8aae135e14e385db786177fc471f7fcdce69
Xingwang Liao <kuoruan@gmail.com>
2019-07-05 16:18:54 +0800

    fix: inbound settings depends

--- luasrc/model/cbi/v2ray/inbound-detail.lua
+++ luasrc/model/cbi/v2ray/inbound-detail.lua
@@ -46,6 +46,7 @@ o = s:option(Flag, "transparent_proxy_enabled", translate("Transparent proxy ena
 o:depends("protocol", "dokodemo-door")
 
 o = s:option(TextValue, "_settings", translate("Settings"), translate("Protocol-specific settings, JSON string"))
+o:depends("transparent_proxy_enabled", "")
 o:depends("transparent_proxy_enabled", "0")
 o.wrap = "off"
 o.rows = 5
