9aa10b794ccdc296ae0e30a809c30a21835b4125
Xingwang Liao <kuoruan@gmail.com>
2020-05-28 11:57:27 +0800

    fix: fix duplicate tabs in 19.07.3
    fix #165

--- public/luasrc/controller/v2ray.lua
+++ public/luasrc/controller/v2ray.lua
@@ -12,23 +12,23 @@ function index()
 
 	entry({"admin", "services", "v2ray"}, firstchild(), _("V2Ray")).dependent = false
 
-	entry({"admin", "services", "v2ray", "global"}, view("v2ray/main"), _("Global Settings"), 1)
+	entry({"admin", "services", "v2ray", "main"}, view("v2ray/main"), _("Global Settings"), 10)
 
-	entry({"admin", "services", "v2ray", "inbounds"}, view("v2ray/inbound"), _("Inbound"), 2).leaf = true
+	entry({"admin", "services", "v2ray", "inbound"}, view("v2ray/inbound"), _("Inbound"), 20).leaf = true
 
-	entry({"admin", "services", "v2ray", "outbounds"}, view("v2ray/outbound"), _("Outbound"), 3).leaf = true
+	entry({"admin", "services", "v2ray", "outbound"}, view("v2ray/outbound"), _("Outbound"), 30).leaf = true
 
-	entry({"admin", "services", "v2ray", "dns"}, view("v2ray/dns"), _("DNS"), 4)
+	entry({"admin", "services", "v2ray", "dns"}, view("v2ray/dns"), _("DNS"), 40)
 
-	entry({"admin", "services", "v2ray", "routing"}, view("v2ray/routing"), _("Routing"), 5)
+	entry({"admin", "services", "v2ray", "routing"}, view("v2ray/routing"), _("Routing"), 50)
 
-	entry({"admin", "services", "v2ray", "policy"}, view("v2ray/policy"), _("Policy"), 6)
+	entry({"admin", "services", "v2ray", "policy"}, view("v2ray/policy"), _("Policy"), 60)
 
-	entry({"admin", "services", "v2ray", "reverse"}, view("v2ray/reverse"), _("Reverse"), 7)
+	entry({"admin", "services", "v2ray", "reverse"}, view("v2ray/reverse"), _("Reverse"), 70)
 
-	entry({"admin", "services", "v2ray", "transparent-proxy"}, view("v2ray/transparent-proxy"), _("Transparent Proxy"), 8)
+	entry({"admin", "services", "v2ray", "transparent-proxy"}, view("v2ray/transparent-proxy"), _("Transparent Proxy"), 80)
 
-  entry({"admin", "services", "v2ray", "about"}, view("v2ray/about"), _("About"), 9)
+  entry({"admin", "services", "v2ray", "about"}, view("v2ray/about"), _("About"), 90)
 
   entry({"admin", "services", "v2ray", "request"}, call("action_request"))
 end
